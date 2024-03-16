350c13ca6cd8febf9eac83c3a6249ae4c9092229
Dean Lourence Barquio <78625-22227854@SERVER2.DOMAIN>
2024-03-16 15:44:29 +0800

    first commit

---
+++ it-sysarch32-lab_78625-activity3/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import Pokedex from './Pokedex';
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Pokedex />
+        </>
+    );
+}
+
+export default App;
+
